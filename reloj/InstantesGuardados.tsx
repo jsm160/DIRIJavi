881347a636e028921289fcfd646e5bf3388b0b56
jsm160 <99259473+jsm160@users.noreply.github.com>
2024-12-05 20:33:03 +0100

    Create InstantesGuardados.tsx

---
+++ reloj/InstantesGuardados.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+interface InstantesGuardadosProps {
+  instantes: string[];
+}
+
+const InstantesGuardados: React.FC<InstantesGuardadosProps> = ({ instantes }) => {
+  return (
+    <div className="instantes-guardados">
+      <h2>Instantes Guardados</h2>
+      <ul>
+        {instantes.map((instante, index) => (
+          <li key={index}>{instante}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default InstantesGuardados;

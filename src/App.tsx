2067f276f2e3b50291f32d2323ff909509fe9c09
Javi <jsm160@gcloud.ua.es>
2025-02-20 20:03:50 +0100

    cambios

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import TaskLogging from './TaskLogging'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [] = useState(0)
 
   return (
     <>
